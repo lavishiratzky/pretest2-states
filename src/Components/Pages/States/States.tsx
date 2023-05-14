acdca0701ea2449dfa054212c111605e79960559
lavi shiratzky <lavi,shiratzky@gmail.com>
2023-05-14 22:24:35 +0300

    searchbox

--- src/Components/Pages/States/States.tsx
+++ src/Components/Pages/States/States.tsx
@@ -7,10 +7,12 @@ import notifyService from "../../../Sercices/Notification Service";
 
 
 function States(): JSX.Element {
+    const [query ,setQuery] =useState<string>("")
     const [states, setStates] = useState<Root2[]>([])
     useEffect(()=>{ 
     axios.get<Root>('https://restcountries.com/v2/all ')
     .then(res =>{setStates(res.data)
+        console.log("rendered")
         notifyService.success('Meou Chtula!!!!!!')})
     
     .catch(err => {console.log(err)
@@ -20,8 +22,16 @@ function States(): JSX.Element {
 },[])
     return (
         <div className="States">
-           
-			{states.map(s=><StateCard key={'state' + s.name} state={s}/>)}
+            <h1> Country <input placeholder ="Enter a Country" onChange = {event => setQuery(event.target.value)} />  </h1>    
+			{states.filter(state =>{
+                if(query===""){
+                    return states
+                }
+                else if (state.name.toLocaleLowerCase().includes(query.toLowerCase())){
+                    return states
+                }
+            })
+            .map(s=><StateCard key={'state' + s.name} state={s}/>)}
         </div>
     );
 }
